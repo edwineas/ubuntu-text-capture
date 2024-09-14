e01c293eeaec1bc0d651236f7a2d347926f17b40
Edwin Alex Shaji <96735074+edwineas@users.noreply.github.com>
2024-09-14 18:20:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ SCRIPT_PATH="$PROJECT_DIR/text_capture.sh"
 setup_venv() {
     if [ ! -d "$VENV_DIR" ]; then
         echo "Creating Python virtual environment..."
-        python3 -m venv $VENV_DIR
+        python3 -m venv venv
     else
         echo "Virtual environment already exists."
     fi
